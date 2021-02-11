7ec9090876fc0e483251656653659d3f1464f140
ignacio-nava <ignan_90@hotmail.com>
2021-02-11 11:27:18 -0300

    Fix path. Endswith '/'

--- multitracks/urls.py
+++ multitracks/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'multitracks'
 urlpatterns = [
     path('', views.MtListView.as_view(), name='all'),
-    path('create', views.MtCreateView.as_view(success_url=reverse_lazy('multitracks:all')), name='mt_create'),
+    path('create/', views.MtCreateView.as_view(success_url=reverse_lazy('multitracks:all')), name='mt_create'),
     path('mt/<int:pk>/favorite', views.AddFavoriteView.as_view(), name='mt_favorite'),
     path('mt/<int:pk>/unfavorite', views.DeleteFavoriteView.as_view(), name='mt_unfavorite'),
 ]
